20bbea1a9ec363f6637ebc9d226953a56e7b6110
AvneetSidhu <avneetsidhu23@gmail.com>
2024-12-20 03:15:13 -0500

    4 letter max words

--- filter.py
+++ filter.py
@@ -2,7 +2,7 @@ def filter_wordList(path, output_file):
     with open("enable.txt", "r") as f:
         words = f.readlines()
     
-    long_words = [word.strip() for word in words if len(word.strip()) >= 6]
+    long_words = [word.strip() for word in words if len(word.strip()) >= 4]
 
     with open(output_file, 'w') as f:
         for word in long_words:
